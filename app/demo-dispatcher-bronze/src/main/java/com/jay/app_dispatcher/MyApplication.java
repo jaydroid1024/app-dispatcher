b7fd5965a271a2d523e8c148abcd73a1db680756
jaydroid1024 <jay_droid@163.com>
2021-02-20 15:44:20 +0800

    demo update

---
+++ app/demo-dispatcher-bronze/src/main/java/com/jay/app_dispatcher/MyApplication.java
@@ -0,0 +1,31 @@
+package com.jay.app_dispatcher;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.qlife.lib_app.appdelegate.AppLifeDispatcher;
+
+public class MyApplication extends Application {
+
+
+    private AppLifeDispatcher applicationDelegate;
+
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        applicationDelegate = new AppLifeDispatcher(this);
+        applicationDelegate.attachBaseContext(base);
+
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("AppDispatcher", "MyApplication, onCreate");
+        applicationDelegate.onCreate(this);
+
+    }
+}
---
+++ app/demo-dispatcher-diamond/src/main/java/com/jay/app_dispatcher/LibNetApp.kt
@@ -0,0 +1,48 @@
+package com.jay.app_dispatcher
+
+import android.app.Application
+import android.util.Log
+import com.qlife.lib_app.appdelegate.AppPriority
+import com.qlife.lib_app.appdelegate.IAppLife
+
+/**
+ * LibNetApp,反射调用
+ *
+ * @author wangxuejie
+ * @version 1.0
+ * @date 2019-10-15 10:57
+ */
+class LibNetApp : IAppLife {
+
+    override fun onCreate(application: Application, processName: String) {
+        Log.d(TAG, "LibNetApp,onCreate")
+        init(application)
+    }
+
+    private fun init(application: Application) {
+        app = application
+        instance = this
+    }
+
+
+    /**
+     * 设置该appLife的优先级，必须设置，否则不会回调
+     */
+    override fun onPriority(): Int {
+        return AppPriority.HIGH_DEFAULT
+    }
+
+    companion object {
+        private const val TAG = "AppDispatcher"
+        private lateinit var instance: LibNetApp
+        private lateinit var app: Application
+
+        fun getInstance(): LibNetApp {
+            return instance
+        }
+
+        fun getApp(): Application {
+            return app
+        }
+    }
+}
